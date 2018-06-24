653478cf4f354b5ff5b3c06764aa911947297788
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-24 13:52:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,8 @@ class Student
      VALUES (?, ?)
    SQL
    DB[:conn].execute(sql, self.name, self.grade)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+   binding.pry
  end
 
 
