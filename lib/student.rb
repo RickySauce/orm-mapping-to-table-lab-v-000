ad43b28f4545e6e31f2e7ee9a3d8354f06b18607
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-24 13:58:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
-  attr_accessor :name, :grade, :attributes
+  attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-    @attributes = attributes
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
   end
 
   def self.create_table
